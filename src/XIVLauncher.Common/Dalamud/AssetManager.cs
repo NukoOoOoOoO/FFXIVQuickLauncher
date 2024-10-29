df8bf4cc370d1770fc0780270d425c6db4dcd575
Tsubasahane <wozaiha@gmail.com>
2024-10-29 23:35:31 +0800

    using System.Linq;

--- src/XIVLauncher.Common/Dalamud/AssetManager.cs
+++ src/XIVLauncher.Common/Dalamud/AssetManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using Serilog;
